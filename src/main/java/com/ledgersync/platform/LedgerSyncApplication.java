d4242937ad82ef94240a6e1760cf55d891aab4ed
gohar14 <goharanwar@hotmail.com>
2025-12-27 14:27:02 +0500

    Initial commit: LedgerSync core with Java 25 and Spring 3.5

---
+++ src/main/java/com/ledgersync/platform/LedgerSyncApplication.java
@@ -0,0 +1,13 @@
+package com.ledgersync.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ledgersync/platform/LedgerSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledgersync.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
